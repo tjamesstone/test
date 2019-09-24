29f56a476fe68219081b7fd2f9b601d9c5377923
Tate Stone <tatejamesstone@gmail.com>
2019-09-24 15:36:25 -0600

    Get server up and running

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const ctrl = require('./controller')
+const massive = require('massive')
+let{SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+
+const app = express()
+
+//Middleware
+app.use(express.json())
+app.use(
+    session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+//endpoints
+
+//massive + app listening
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('Database is set')
+    app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}`))
+})
