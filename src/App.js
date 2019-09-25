50728b6c14e6de1fa162543ec66d010cf94c18c6
Tate Stone <tatejamesstone@gmail.com>
2019-09-25 15:21:18 -0600

    More work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Nav from './Components/Nav/Nav';
-import Auth from './Components/Auth/Auth'
-import Form from './Components/Form/Form'
-import Post from './Components/Post/Post'
+import routes from './routes'
 
 function App() {
   return (
     <div className="App">
-      {/* <Auth /> */}
-      {/* <Dashboard /> */}
-      <Form />
-      {/* <Post /> */}
-      <Nav />
+      {routes}
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Auth from './Components/Auth/Auth'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+import Post from './Components/Post/Post'
+import Nav from './Components/Nav/Nav';
+
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Auth}/>
+        <Route path='/dashboard' component={Dashboard}/>
+        <Route path='/post/:postid' component={Post}/>
+        <Route path='/new' component={Form}/>
+    </Switch>
+)
